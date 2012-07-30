ad2cd718b9f587af3f6b9434851d1c8da687004d
Ovidiu Spatacian-Tarnu <ovimacbook@ovispc.home>
2012-07-30 22:27:09 +0100

    It's coming along

--- js/app.js
+++ js/app.js
@@ -6,8 +6,11 @@ Flm = Ember.Application.create({
 	author: "Ovidiu",
 	rootElement: "#page",
 	ready: function() {
+		Flm.filteredListController.ready();
 	}
 });
+
+Flm.initialize();
 
 
 /****************************
@@ -29,43 +32,103 @@ Flm = Ember.Application.create({
   * Views
 ****************************/
 
+Flm.FilmFilter = Ember.TextField.extend({
+	change: function() {
+		var value = this.get("value");
+		console.log(value);
+		this.set("value", value);
+		var filter = value.replace(/^\s+||\s+$/g,'');
+		Flm.filteredListController.filter(filter);
+	}.observes("value")
+});
+
+Flm.FilmListView = Ember.View.extend({
+	ready: function() {
+		var me = this;
+		$(me).children();
+	},
+	mouseEnter: function(event) {
+		var ul = $(event.currentTarget).children("ul");
+	}
+});
+
+Flm.FilmListItemView = Ember.View.extend({
+	click: function() {
+		var film = this.content;
+		Flm.filmDisplay.addItem(film);
+		return false;
+	}
+});
+
+// This is the view for the image tag
+Flm.FilmView = Ember.View.extend({
+	ready: function() {
+		var me = this;
+	}
+});
+
 
 /****************************
   * Controllers
 ****************************/
 
+
+
 Flm.listController = Ember.ArrayController.create({
 	content: [],
-	loadFilm: function(event) {
-		event.preventDefault();
-		var filmDescription = event.context;
-		Flm.filmDisplay.addItem(filmDescription);
+	ready: function () {
+		var me = this;
+		$.getJSON("data/films.js", function(data) {
+			$.each(data, function(index, film) {
+				var rottenTommatoesID = film.rottenTommatoesID;
+				var urlJSON = "http://api.rottentomatoes.com/api/public/v1.0/movies/" + rottenTommatoesID + ".json?apikey=2k5j37rmsuw3duzyua4bcnek";
+				$.ajax({
+					url: urlJSON,
+					dataType: "jsonp",
+					success: result
+				});
+				function result(filmInfo) {
+					if (film.synopsis == "") {
+						film.synopsis = filmInfo.synopsis;
+					}
+					film.thumb = filmInfo.posters.profile;
+					film.poster = filmInfo.posters.original;
+					me.pushObject(film); // it's important that an object is pushed to the content of this controller from the ajax success function otherwise the external json content may not load.
+				}
+			});
+			
+		});
+		var myContent = me.get("content");
+		console.log(myContent);
 	}
 });
 
-$.getJSON("data/films.js", function(data) {
-	$.each(data, function(index, film) {
-		var rottenTommatoesID = film.rottenTommatoesID;
-		var urlJSON = "http://api.rottentomatoes.com/api/public/v1.0/movies/" + rottenTommatoesID + ".json?apikey=2k5j37rmsuw3duzyua4bcnek";
-		$.ajax({
-			url: urlJSON,
-			dataType: "jsonp",
-			success: result
-		});		
-		function result(filmInfo) {
-			if (film.synopsis == "") {
-				film.synopsis = filmInfo.synopsis;
-			}
-			film.poster = filmInfo.posters.detailed;
-		}
-	});
-	Flm.listController.set("content", data);
+Flm.filteredListController = Ember.ArrayController.create({
+	content: [],
+	ready: function() {
+		Flm.listController.ready();
+		var originalFilmList = Flm.listController.get("content");
+		this.pushObjects(originalFilmList);
+	},
+	filter: function(filter) {
+		//this.set("content", "");
+		var pattern = new RegExp(filter, "i");
+		
+		var originalFilmList = Flm.listController.get("content");
+		var filteredFilmList = $.grep(originalFilmList, function(element,index) {
+			return element.title.match(pattern);
+		});
+		this.set("content", filteredFilmList);
+	}
 });
+
+
 
 Flm.filmDisplay = Ember.ArrayController.create({
 	content: [],
-	addItem: function(filmDescription) {
+	addItem: function(film) {
+		var me = this;
 		if ( this.get('length') ) this.removeAt( 0, this.get('length') );
-		this.pushObject(filmDescription);
-	}
+		this.pushObject(film);
+	}	
 });
